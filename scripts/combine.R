abcdcec25b174ffbd2f2d62d4594e982bc5f1941
Patrick Short <ps14@sanger.ac.uk>
2015-05-22 09:38:12 +0100

    combine many simulation chunk analyses

---
+++ scripts/combine.R
@@ -0,0 +1,27 @@
+# combine set of annotated dataframes with the same headers (simulation data)
+
+library(optparse)
+
+### command line options
+option_list <- list(
+  make_option("--base_name", default="/tmp/denovoLOBGOB_sim_chunks/sim_data",
+              help="Pass the base name of the chunks."),
+  make_option("--n_chunks", default=1000,
+              help="Number of chunks that should be combined."),
+  make_option("--out", default="../results/simulated_de_novos_JASPAR_tfbs_annotation.txt",
+              help="Set location to save the output of combined chunks.")
+)
+
+args <- parse_args(OptionParser(option_list=option_list))
+
+chunks = vector("list", args$n_chunks)
+for (i in seq(args$n_chunks)){
+  fname = sprintf("%s.%i.txt", args$base_name, i) # will look for fname e.e.g /tmp/denovoLOBGOB_sim_chunks/sim_data.n.out for n 1-n_chunks
+  chunks[[i]] = read.table(fname, sep = "\t", header = TRUE)
+}
+
+annotated_sim <- do.call(rbind, chunks)
+annotated_sim$diff <- annotated_sim$ref_score - annotated_sim$alt_score
+
+write.table(annotated_sim, file = args$out, quote=FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+
